60d5312d2a7cb4246bcd0b97b66ce4928d51f2a4
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-31 16:13:24 +0100

    2:25:22

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,24 @@
+'use client'
+
+import Image from "next/image"
+import { useUser } from "@clerk/clerk-react"
+import { Button } from "@/components/ui/button"
+import { PlusCircle } from "lucide-react"
+
 export default function DocumentsPage () {
+
+  const {user} = useUser()
+
 return (
-    <div>
-      This is protected page
+    <div className="flex flex-col justify-center items-center h-full space-y-4">
+      <Image className="dark:hidden" src='empty.png' alt="Empty" width='300' height='300'/>
+      <Image className="hidden dark:block" src='empty-dark.png' alt="Empty" width='300' height='300'/>
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotion
+      </h2>
+      <Button>
+        <PlusCircle className="w-4 h-4"/>
+      </Button>
     </div>
 )
 }
