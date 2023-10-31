7db887c78d6dd30fb89413860707f7944f7d328c
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-31 16:31:19 +0100

    2:32:56

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -16,7 +16,16 @@ export function UserItem () {
 return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-
+        <div className="flex items-center text-sm p-3 w-full hover:bg-primary/5" role="button">
+          <div className="gap-x-2 flex items-center max-w-[150px]">
+            <Avatar className="w-5 h-5">
+              <AvatarImage src={user?.imageUrl}/>
+            </Avatar>
+              <span className="text-start font-medium line-clamp-1">
+                {user?.fullName}&apos;s Jotion
+              </span>
+          </div>
+        </div>
       </DropdownMenuTrigger>
     </DropdownMenu>
 )
