b32132c53e603439f44fb7e9cee1cee42368d9d4
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-01 17:10:40 +0100

    6:15:18
    add icon and remove icon UI and logic

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -23,6 +23,7 @@ export function Toolbar ({initialData,preview}:ToolbarProps) {
   const [value,setValue] = useState(initialData.title)
 
   const update = useMutation(api.documents.update)
+  const removeIcon = useMutation(api.documents.removeIcon)
 
   const enableInput = () => {
     if (preview) return
@@ -51,15 +52,28 @@ export function Toolbar ({initialData,preview}:ToolbarProps) {
     }
   }
 
+  const onIconSelect = (icon:string) => {
+    update({
+      id:initialData._id,
+      icon,
+    })
+  }
+
+  const onRemoveIcon = () => {
+    removeIcon({
+      id:initialData._id
+    })
+  }
+
 return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
         <div className="flex gap-x-2 items-center group/icon pt-6">
-          <IconPicker onChange={() => {}}>
+          <IconPicker onChange={onIconSelect}>
             <p className="text-6xl hover:opacity-75 transition">{initialData.icon}</p>
           </IconPicker>
           <Button className="rounded-full opacity-0 group-hover/icon:opacity-100 transition
-          text-muted-foreground text-xs" variant='outline' size='icon' onClick={() => {}}>
+          text-muted-foreground text-xs" variant='outline' size='icon' onClick={onRemoveIcon}>
             <X className="w-4 h-4"/>
           </Button>
         </div>
@@ -71,7 +85,7 @@ return (
       )}
       <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
         {!initialData.icon && !preview && (
-          <IconPicker asChild onChange={() => {}}>
+          <IconPicker asChild onChange={onIconSelect}>
             <Button className="text-muted-foreground text-xs" variant='outline' size='sm'>
               <Smile className="w-4 h-4 mr-2"/>
               Add icon
--- convex/documents.ts
+++ convex/documents.ts
@@ -294,3 +294,33 @@ export const update = mutation({
     return document
   }
 })
+
+
+export const removeIcon = mutation({
+  args:{id:v.id('documents')},
+  handler:async (context,args) => {
+      const identity = await context.auth.getUserIdentity()
+
+    if (!identity) {
+      throw new Error("Unauthenticated")
+    }
+
+    const userId = identity.subject
+
+     const existingDocument = await context.db.get(args.id)
+
+    if (!existingDocument) {
+      throw new Error('Not found')
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized")
+    }
+
+    const document = await context.db.patch(args.id,{
+      icon:undefined
+    })
+
+    return document
+  } 
+})
