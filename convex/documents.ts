b5d64ec793bb3d5a3ae81a1ff543d6e8cf836bf7
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-01 11:24:55 +0100

    4:11:55

--- convex/documents.ts
+++ convex/documents.ts
@@ -110,5 +110,98 @@ export const getTrash = query({
     }
 
     const userId = identity.subject
+
+    const documents = await context.db.query('documents')
+    .withIndex('by_user',q => q.eq('userId',userId))
+    .filter(q => q.eq(q.field('isArchived'),true))
+    .order('desc')
+    .collect()
+
+    return documents
+  }
+})
+
+export const restore = mutation({
+  args:{id:v.id('documents')},
+  handler: async (context,args) => {
+    const identity = await context.auth.getUserIdentity()
+
+    if (!identity) {
+      throw new Error('Not authenticated')
+    }
+
+    const userId = identity.subject
+
+    const existingDocument = await context.db.get(args.id)
+
+    if (!existingDocument) {
+      throw new Error('Not found')
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized")
+    }
+
+    const recursiveRestore = async (documentId:Id<'documents'>) => {
+      const children = await context.db.query('documents')
+      .withIndex('by_user_parent',q => (
+        q.eq('userId',userId).eq('parentDocument',documentId)
+      ))
+      .collect()
+
+      for (const child of children) {
+        await context.db.patch(child._id,{
+          isArchived:false
+        })
+
+        await recursiveRestore(child._id)
+      }
+    }
+
+    const options:Partial<Doc<'documents'>> = {
+      isArchived:false
+    }
+
+    if (existingDocument.parentDocument) {
+      const parent = await context.db.get(existingDocument.parentDocument)
+      if (parent?.isArchived) {
+        options.parentDocument = undefined
+      }
+    }
+
+    const document = await context.db.patch(args.id,options)
+
+    recursiveRestore(args.id)
+
+    return document
+  }
+})
+
+
+export const remove = mutation({
+  args:{id:v.id('documents')},
+  handler:async (context,args) => {
+
+    const identity = await context.auth.getUserIdentity()
+
+    if (!identity) {
+      throw new Error('Not authenticated')
+    }
+
+    const userId = identity.subject
+
+    const existingDocument = await context.db.get(args.id)
+
+    if (!existingDocument) {
+      throw new Error('Not found')
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Unauthorized")
+    }
+
+    const document = await context.db.delete(args.id)
+
+    return document
   }
 })
