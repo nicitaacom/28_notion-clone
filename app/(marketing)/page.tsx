537de7e37ed6128553cb3fa4e61e01198c2105fc
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-01 20:29:32 +0100

    / route bg fix

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import { Heroes } from "./_components/Heroes";
 
 export default function MarketingPage () {
 return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center
       md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
           <Heading/>
