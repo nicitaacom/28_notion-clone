3c61b2ed14457aed6b69d3cf7df218da937e1677
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-01 19:10:08 +0100

    7:11:52
    logic to upload images in note

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -5,6 +5,8 @@ import {BlockNoteView,useBlockNote} from '@blocknote/react'
 import '@blocknote/core/style.css'
 import { useTheme } from "next-themes"
 
+import { useEdgeStore } from "@/lib/edgestore"
+
 interface EditorProps{
   onChange:(value:string) => void
   initialContent?:string
@@ -14,13 +16,21 @@ interface EditorProps{
 export function Editor ({onChange,initialContent,editable}:EditorProps) {
 
   const {resolvedTheme} = useTheme()
+  const {edgestore} = useEdgeStore()
+
+  const handleUpload = async (file:File) => {
+    const response = await edgestore.publicFiles.upload({file})
+
+    return response.url
+  }
 
   const editor:BlockNoteEditor = useBlockNote({
     editable,
     initialContent:initialContent ? JSON.parse(initialContent) as PartialBlock[] : undefined,
     onEditorContentChange:(editor) => {
       onChange(JSON.stringify(editor.topLevelBlocks,null,2))
-    }
+    },
+    uploadFile:handleUpload
   })
 
 return (
