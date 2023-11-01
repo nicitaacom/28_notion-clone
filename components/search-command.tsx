6d7855f699c95588961d99349542075267afb939
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-01 12:37:58 +0100

    4:44:36
    UI and some logic for search

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -27,13 +27,50 @@ export function SearchCommand () {
     setIsMounted(true)
   },[])
 
+  useEffect(() => {
+    const down = (e:KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        toggle()
+      }
+    }
+    document.addEventListener('keydown',down)
+    return () => document.removeEventListener('keydown',down)
+  },[toggle])
+
+  const onSelect = (id:string) => {
+    router.push(`/documents/${id}`)
+    onClose()
+  }
+
   if (!isMounted) {
     return null
   }
 
   return (
     <CommandDialog open={isOpen} onOpenChange={onClose}>
-
+      <CommandInput placeholder={`Search ${user?.fullName}'s Jotion`}>
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading='Documents'>
+          {documents?.map(document => (
+            <CommandItem key={document._id} value={`${document._id}-${document.title}`}
+            title={document.title} onSelect={onSelect}>
+              {document.icon ? (
+                <p className="mr-2 text-[18px]">
+                  {document.icon}
+                </p>
+              ) : (
+                <File className="w-4 h-4 mr-2"/>
+              )}
+              <span>
+                {document.title}
+              </span>
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+      </CommandInput>
     </CommandDialog>
 )
 }
