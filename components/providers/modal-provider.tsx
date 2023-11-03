f8a55e0af5e055595896c3f70e3b2bd149584528
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-03 19:30:43 +0100

    imports separated

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,7 +1,8 @@
 'use client'	
 
-import { SettingsModal } from "@/components/modals/settings-modal"
 import { useEffect, useState } from "react"
+
+import { SettingsModal } from "@/components/modals/settings-modal"
 import { CoverImageModal } from "@/components/modals/cover-image-modal"
 
 export function ModalProvider () {
